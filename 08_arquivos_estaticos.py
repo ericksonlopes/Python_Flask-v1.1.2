41b4bc81652235bff6853421c4d64c62d679dfb1
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-14 10:26:29 -0300

    08

---
+++ 08_arquivos_estaticos.py
@@ -0,0 +1,15 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('08_hello.html', name=name)
+
+
+# O Flask procurará modelos na templates pasta. Portanto, se seu aplicativo é um módulo,
+# esta pasta está ao lado desse módulo, se for um pacote, ela está realmente dentro do seu pacote:
+
+app.run()
